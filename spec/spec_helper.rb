ec18b7ab329c4b1c66af9967b0832206ba288291
James Moriarty <james.moriarty@rea-group.com>
2015-07-09 08:49:02 +1000

    code climate coverage.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,7 @@
-require 'pry'
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
+
+require "pry"
 
 ["simplecov", "repository"].each do |file|
   require File.dirname(__FILE__) + "/support/#{file}"
