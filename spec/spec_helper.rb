807852b2c28fba345042f2b56a4b40233c22eab8
James Moriarty <jamespaulmoriarty@gmail.com>
2019-08-25 20:51:49 +1000

    bugfix: old deps.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,3 @@
-require "codeclimate-test-reporter"
-CodeClimate::TestReporter.start
-
-require "pry"
-
 ["simplecov", "repository"].each do |file|
   require File.dirname(__FILE__) + "/support/#{file}"
 end
